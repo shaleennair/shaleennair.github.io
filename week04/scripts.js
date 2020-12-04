1851b7756397fd60956f5538b32b65076047b21b
Shaleen Nair <70945468+shaleennair@users.noreply.github.com>
2020-12-04 20:48:20 +0800

    0848

--- week04/scripts.js
+++ week04/scripts.js
@@ -5,3 +5,14 @@ function transformUppercase(){
 
 let elUsername = document.getElementById("namaPengguna");
 elUsername.addEventListener("keyup", transformUppercase);
+
+function checkPasswordLength(){
+    let password = document.getElementById("kataLaluan").value;
+    if (password.length < 5) {
+        alert("Password must be more than 5 characters");
+        } else {
+            alert("Password accepted!")
+        }
+    }
+let elPassword = document.getElementById("kataLaluan");
+elPassword.onblur = checkPasswordLength;
