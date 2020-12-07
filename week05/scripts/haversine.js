4fd73e7a90c9f43c4962a1ddd226cd067dd3125b
Shaleen Nair <70945468+shaleennair@users.noreply.github.com>
2020-12-07 21:56:57 +0800

    0956

--- week05/scripts/haversine.js
+++ week05/scripts/haversine.js
@@ -3,7 +3,7 @@ function haversine(lat1, lon1, lat2, lon2) {
     let dLat = (lat2-lat1).toRad();
     let dLon = (lon2-lon1).toRad();
     lat1 = lat1.toRad();
-    lat2 = lat2.toRad();
+    lat2 = lat2.toRad(); //test
 
     let a = Math.sin(dLat/2) * Math.sin(dLat/2) + Math.sin(dLon/2) * Math.cos(lat1) * Math.cos(lat2);
     let c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a));
