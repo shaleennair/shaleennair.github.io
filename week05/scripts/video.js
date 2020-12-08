b00a4fb461f3685c9f7e7790a0c2343283f858a5
Shaleen Nair <70945468+shaleennair@users.noreply.github.com>
2020-12-08 20:57:44 +0800

    0857

---
+++ week05/scripts/video.js
@@ -0,0 +1,22 @@
+window.onload =function(){
+    let video = document.getElementById("myVideo");
+
+    let playBtn = document.getElementById("playBtn");
+    playBtn.addEventListener("click", function() {
+        video.play();
+    });
+
+    let pauseBtn = document.getElementById("pauseBtn");
+    pauseBtn.addEventListener("click", function () {
+        video.pause();
+    });
+
+    let freezeBtn = document.getElementById("freezeBtn");
+    freezeBtn.addEventListener("mousedown", function () {
+        video.pause();
+    });
+    freezeBtn.addEventListener("mouseup", function () {
+        video.play();
+    });
+    
+};
