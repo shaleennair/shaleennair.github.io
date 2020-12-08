8f5eb4222ca05061dd22075512219f7b3d71c07d
Shaleen Nair <70945468+shaleennair@users.noreply.github.com>
2020-12-08 21:18:48 +0800

    0918

--- week05/scripts/video.js
+++ week05/scripts/video.js
@@ -18,5 +18,16 @@ window.onload =function(){
     freezeBtn.addEventListener("mouseup", function () {
         video.play();
     });
+
+    let seekBar = document.getElementById("seekBar");
+    seekBar.addEventListener("change", function() {
+        let time = video.duration * (seekBar.value / 100);
+        video.currentTime = time;
+    });
+
+    video.addEventListener("timeupdate", function() {
+        let value = (100 / video.duration) * video.currentTime;
+        seekBar.value = value;
+    });
     
 };
