012d4b1916d2143a348353a89a81835719f3c68b
Shaleen Nair <70945468+shaleennair@users.noreply.github.com>
2020-12-11 21:15:46 +0800

    0915

--- week06/scripts/ajax.js
+++ week06/scripts/ajax.js
@@ -12,6 +12,8 @@ function getRandomUser() {
             let elAddress = document.getElementById("address");
             let elEmail = document.getElementById("email");
 
+            //test
+
             elFirstName.innerHTML = data.results[0].name.first;
             elUserImage.src = data.results[0].picture.large;
             elUserImage.title = data.results[0].name.first + " " + data.result[0].name.elLastName
