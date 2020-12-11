4bc0e14953106c838dcc30538f16eaa4e9be4d53
Shaleen Nair <70945468+shaleennair@users.noreply.github.com>
2020-12-11 20:59:33 +0800

    0859

--- week06/scripts/ajax.js
+++ week06/scripts/ajax.js
@@ -12,6 +12,8 @@ function getRandomUser() {
             let elAddress = document.getElementById("address");
             let elEmail = document.getElementById("email");
 
+            elFirstName.innerHTML = data.results[0].name.first;
+
             elJsonResult.innerHTML = this.response;
         }
     }
