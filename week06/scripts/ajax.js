bd6240eb1c86d86a2e0c314e8db6e4089d9d6d0a
Shaleen Nair <70945468+shaleennair@users.noreply.github.com>
2020-12-11 20:37:54 +0800

    0837

---
+++ week06/scripts/ajax.js
@@ -0,0 +1,26 @@
+function getRandomUser() {
+    let xhttp = new XMLHttpRequest(); //create request obj
+
+    xhttp.onreadystatechange = function() {
+        if(this.readyState == 4 && this.status == 200) {
+            let data = JSON.parse(this.response);
+            let elFirstName = document.getElementById("firstName");
+            let elLastName = document.getElementById("lastName");
+            let elJsonResult = document.getElementById("jsonResult");
+            let elUserImage = document.getElementById("userImage");
+            let elPhone = document.getElementById("phone");
+            let elAddress = document.getElementById("address");
+            let elEmail = document.getElementById("email");
+
+            elJsonResult.innerHTML = this.response;
+        }
+    }
+
+    xhttp.open("GET", "https://randomuser.me/api", true);
+    xhttp.send();
+}
+
+let elGetRandomUser = document.getElementById("getRandomUserBtn")
+elGetRandomUser.addEventListener("click", function () {
+    getRandomUser();
+});
