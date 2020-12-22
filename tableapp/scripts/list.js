d2eacacd4ba369f04e34daf07bdf196878af8310
Shaleen Nair <70945468+shaleennair@users.noreply.github.com>
2020-12-22 21:25:28 +0800

    0925

--- tableapp/scripts/list.js
+++ tableapp/scripts/list.js
@@ -33,7 +33,7 @@ function GetBookings() {
                 let el = document.getElementById(bookingIds[j]);
                 el.addEventListener("click", function () {
                     let theId = bookingIds[j].replace("delete", "");
-                    alert(theId);
+                    DeleteBooking(theId);
                 });
             }
         });
