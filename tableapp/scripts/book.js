a6553fc5d5d8849f18fdcb33e3c2cb35d1b2447c
Shaleen Nair <70945468+shaleennair@users.noreply.github.com>
2020-12-22 20:09:42 +0800

    0809

---
+++ tableapp/scripts/book.js
@@ -0,0 +1,35 @@
+function BookNow(guestName, guestEmail, guestPax, guestRemarks) {
+    let url = 'https://api.sheety.co/08659f8d3caa9d98dd13a805222492d5/bookingApp/bookings';
+    let body = {
+        booking: {
+            name: guestName,
+            email: guestEmail,
+            pax: guestPax
+            remarks: guestRemarks
+        }
+    }
+    fetch(url, {
+        method: 'POST',
+        body: JSON.stringify(body),
+        headers: {
+            "Content-Type": "application/json"
+        }
+    })
+        .then((response) => response.json())
+        .then(json => {
+            alert(json.booking.name + " added!");
+
+        });
+}
+
+window.addEventListener("load", function () {
+    document.getElementById("bookNow").addEventListener("click", function () {
+        let userName = document.getElementById("userName").value;
+        let userEmail = document.getElementById("userName").value;
+        let userPax = document.getElementById("userName").value;
+        let userRemarks = document.getElementById("userName").value;
+
+        BookNow(userName, userEmail, userPax, userRemarks);
+
+    });
+});
